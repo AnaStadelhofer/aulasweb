ae49a3b2d3cbfe0bbf98e7f9e0f7250704a472a3
Ana Stadelhofer <ana.stadelhofer@outlook.com>
2022-09-08 21:25:33 -0300

    commit

---
+++ exercicios/exeercicio01.1/nav.php
@@ -0,0 +1,8 @@
+<nav class="main">
+    <ul class="container">
+        <li><img src="https://picsum.photos/100/70"></li>
+        <li><a href="index.php">Inicial</a></li>
+        <li><a href="sobre.php">Sobre</a></li>
+        <li><a href="contato.php">Contato</a></li>
+    </ul>
+</nav>
